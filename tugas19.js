355b778a6cac4d59596422d280d552f1102c04a5
susila tropika <64695610+susilaattari@users.noreply.github.com>
2021-09-10 07:42:21 +0700

    Tugas 19 Javascript niomic

---
+++ tugas19.js
@@ -0,0 +1,17 @@
+let kata = 'Semangat....'
+console.log(kata.repeat(5))
+// console.log('*'.repeat(5))
+// console.log('*'.repeat(5))
+let a = 0
+for(i = 0 ; i<4 ; i++){
+    let bintang = '*'
+    a = a + 10 
+    console.log(bintang.repeat(a))
+}
+
+
+for(i = 0 ; i<3 ; i++){
+    let bintang = '*'
+    a = a - 10 
+    console.log(bintang.repeat(a))
+}
